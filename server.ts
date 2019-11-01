1146e3af44660655139b084c43551a56be406560
Rodrigo Crespillio <emperorrc@gmail.com>
2019-11-01 00:45:52 -0300

    improved script live reload backend

--- server.ts
+++ server.ts
@@ -23,38 +23,45 @@ import api from './server/api';
 
 import {join} from 'path';
 
+const myArgs = process.argv.slice(2) || [];
+const serverOnly = myArgs.includes('serverOnly');
+
 // Express server
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+if(!serverOnly){
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
 
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+  const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
+  // * NOTE :: leave this as require() since this file is built Dynamically from webpack
+  const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  app.engine('html', ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  }));
+
+  app.set('view engine', 'html');
+  app.set('views', DIST_FOLDER);
+  // Serve static files from /browser
+  app.get('*.*', express.static(DIST_FOLDER, {
+    maxAge: '1y'
+  }));
+
+  // All regular routes use the Universal engine
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.render('index', { req });
+  });
+}
 // Express Rest API endpoints
 app.use('/api/', api);
-// Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
-
-// All regular routes use the Universal engine
-
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.render('index', { req });
-});
 
 // Start up the Node server
 app.listen(PORT, () => {
